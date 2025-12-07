e40deeca5f88eb52e861bc9397d5bb6ac42ccb71
meisamkassaeian <80329359+meisamkassaeian@users.noreply.github.com>
2025-12-07 11:23:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import os
 from flask import Flask, request
-from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler
 from telegram import Bot, Update
-from bot import add_product, calculate_price, set_dirham
+from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler
+from bot import add_and_send_product, calculate_price, set_dirham
 
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # مثل https://mybot.onrender.com/webhook
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = Bot(TOKEN)
 app = Flask(__name__)
 
-# Dispatcher
 dp = Dispatcher(bot, None, workers=0)
-dp.add_handler(CommandHandler("setdirham", set_dirham))
-dp.add_handler(CommandHandler("addproduct", add_product))
+dp.add_handler(CommandHandler("addproduct", add_and_send_product))
 dp.add_handler(CallbackQueryHandler(calculate_price))
-
 
 @app.route("/")
 def home():
     return "Bot is running."
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -28,13 +24,11 @@ def webhook():
     dp.process_update(update)
     return "OK", 200
 
-
 @app.route("/setwebhook")
 def set_webhook():
     url = f"{WEBHOOK_URL}/webhook"
     bot.set_webhook(url)
     return f"Webhook set to {url}"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
