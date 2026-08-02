5152068dd30925c9cf4dcdfa02c95a96d1a92bbf
meisamkassaeian <80329359+meisamkassaeian@users.noreply.github.com>
2026-08-02 16:22:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -40,15 +40,4 @@ def set_webhook():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/debug")
-def debug():
-    return "DEBUG OK"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
 
